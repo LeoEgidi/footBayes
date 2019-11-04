271db5dcfa54c86e075624479f01d9eebf6e6d7e
Leonardo Egidi <legidi@units.it>
2019-11-04 12:07:20 +0100

    Update fit_foot.R

--- R/fit_foot.R
+++ R/fit_foot.R
@@ -1,6 +1,7 @@
 #' Fit any football model
 #'
-#'
+#'@param data a data frame, or a matrix containing the following mandatory items: home team, away team,
+#'home goals, away goals.
 
 
 fit_foot <- function(data, model){
