9b4e474fec00e26f20dfbe94a7dbe895999afe8f
Leonardo Egidi <legidi@units.it>
2020-01-02 17:09:37 +0100

    fff

--- R/pp_foot.R
+++ R/pp_foot.R
@@ -2,7 +2,7 @@ pp_foot <- function(data, object,
                     type = c("aggregated", "matches")){
 
   sims <- rstan::extract(object)
-  y <- data[,4:5]
+  y <- as.matrix(data[,4:5])
   diff_gol <- as.vector(y[,1] - y[,2])
   diff_gol_rep <- sims$diff_y_rep
   esiti_short <- seq(-3,3,1)
@@ -10,6 +10,9 @@ pp_foot <- function(data, object,
   freq_rel_matrix <- matrix(NA, M, length(esiti_short))
   ngames_train <- dim(sims$y_rep)[2]
 
+  if (missing(type)){
+    type <- "aggregated"
+  }
   if (type =="aggregated"){
 
   for (j in 1:M){
@@ -56,7 +59,53 @@ pp_foot <- function(data, object,
           axis.text.x = element_text(size=15),
           axis.text.y = element_text(size=15))
   }else if (type=="matches"){
+    scd <- as.numeric(as.vector(diff_gol))
+    scd_sims <- diff_gol_rep
+    scd_hat <- colMedians(scd_sims)
+    scd_se <- sqrt(colVars(scd_sims))
+    alpha <- 0.95;
+    scd_ub <- colQuantiles(scd_sims, probs = 1-(1-alpha)/2)
+    scd_lb <- colQuantiles(scd_sims, probs = (1-alpha)/2)
+    ci95 <- sum(scd < scd_ub & scd_lb<scd)/ngames_train
+    ngames_train_draw <- sum(scd ==0)
+    scd_draw <- scd[scd==0]
+    ci95_draw <- sum(scd_draw < scd_ub[scd==0] & scd_lb[scd==0]<scd_draw)/ngames_train_draw
+    alpha <- 0.5;
+    scd_ub2 <- colQuantiles(scd_sims, probs = 1-(1-alpha)/2)
+    scd_lb2 <- colQuantiles(scd_sims, probs = (1-alpha)/2)
+    ci50 <- sum(scd < scd_ub2 & scd_lb2<scd)/ngames_train
+    ci50_draw <- sum(scd_draw < scd_ub2[scd==0] & scd_lb2[scd==0]<scd_draw)/ngames_train_draw
 
+    sort_scd <- scd[order(scd)]
+    sort_scd_hat <- scd_hat[order(scd)]
+    sort_scd_se <- scd_se[order(scd)]
+    sort_scd_ub <- scd_ub[order(scd)]
+    sort_scd_lb <- scd_lb[order(scd)]
+    sort_scd_ub2 <- scd_ub2[order(scd)]
+    sort_scd_lb2 <- scd_lb2[order(scd)]
+
+    df <- data.frame(list(scd = sort_scd, scd_hat = sort_scd_hat, scd_se = sort_scd_se,
+                          scd_ub = sort_scd_ub, scd_lb = sort_scd_lb,
+                          scd_ub2 = sort_scd_ub2, scd_lb2 = sort_scd_lb2))
+
+ggplot(df, aes(x = c(1:ngames_train))) +
+      geom_ribbon(aes(ymin = scd_lb, ymax = scd_ub),
+                  fill="#F0E442") +
+      geom_ribbon(aes(ymin = scd_lb2, ymax = scd_ub2),
+                  fill="khaki3") +
+      #geom_line(aes(y=scd_hat),colour="darkred") +
+      #geom_point(aes(y=scd_hat),colour="darkred",shape=4) +
+      geom_point(aes(y=scd), size = 0.5, col="blue") +
+      scale_x_continuous(name="games") +
+      #scale_y_discrete(name="score difference", limits=seq(-8,8)) +
+      scale_y_continuous(name="Goal difference",
+                  breaks = c(-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8),
+                  sec.axis = dup_axis()) +
+      yaxis_text(size=rel(1.4))+
+      xaxis_text( size = rel(1.4))+
+      theme(axis.title=element_text(size=19),
+      axis.text.x = element_text(size=15),
+      axis.text.y = element_text(size=15))
   }
 
 }
