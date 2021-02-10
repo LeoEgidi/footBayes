4e54c3bf17c1a77113c968c39fad489aa06a63b9
Leonardo Egidi <legidi@units.it>
2021-02-10 17:51:36 +0100

    hh

---
+++ tests/testthat/test_foot_abilities.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(engsoccerdata)
+
+##########################
+## DATA
+#########################
+
+england <- as_tibble(england)
+# one season only
+england_2004 <- england %>%
+  dplyr::select(Season, home, visitor, hgoal,vgoal) %>%
+  filter(  Season=="2004")
+
+# more seasons
+england_1999_2001 <- england %>%
+  dplyr::select(Season, home, visitor, hgoal,vgoal) %>%
+  filter(  Season=="2001" | Season == "2000" | Season =="1999")
+
+
+context("static models")
+test_that("object argument gives errors/warnings", {
+
+  # the class of the model is not stanfit and not
+  # from optimization methods (mle)
+  y <- england_2004[,2]
+  fit <- lm(y~ rnorm(length(y)))
+  expect_error(foot_abilities(fit, england_2004))
+
+
+})
+
+context("dynamic models")
