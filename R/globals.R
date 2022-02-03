57bb765194ab560954f2a639e5386656f1d3e54b
Leonardo Egidi <legidi@units.it>
2022-02-03 01:02:57 +0100

    Update globals.R

--- R/globals.R
+++ R/globals.R
@@ -9,7 +9,8 @@ utils::globalVariables(c('median', 'quantile', 'group_by', 'summarise',
                        'Home', 'Away', 'Prob', 'rpois', 'lo2', 'hi2',
                        'q_025', 'q_975', 'q_25', 'q_75', 'q_50',
                        'filter', 'Home_prob', '.','dpois', 'optim', 'pchisq',
-                       'as_nlist', 'valori', 'mutate', 'times'))
+                       'as_nlist', 'valori', 'mutate', 'times', '%>%',
+                       'day'))
 
 #LinkingTo: StanHeaders (>= 2.18.0), rstan (>= 2.18.1), BH (>= 1.66.0),
 #Rcpp (>= 0.12.0), RcppEigen (>= 0.3.3.3.0)
