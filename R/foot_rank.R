c00ef661aac20e3ed103de69796f5248241df592
Leonardo Egidi <legidi@units.it>
2023-08-30 20:58:52 +0200

    Update foot_rank.R

--- R/foot_rank.R
+++ R/foot_rank.R
@@ -27,13 +27,14 @@
 #'
 #' \donttest{
 #' require(tidyverse)
+#' require(dplyr)
 #'
 #' data("italy")
 #' italy_1999_2000<- italy %>%
 #' dplyr::select(Season, home, visitor, hgoal,vgoal) %>%
 #' dplyr::filter(Season == "1999"|Season=="2000")
 #'
-#' fit <- stan_foot(italy_2000, "double_pois", iter = 200)
+#' fit <- stan_foot(italy_1999_2000, "double_pois", iter = 200)
 #' foot_rank(italy_1999_2000, fit)
 #' foot_rank(italy_1999_2000, fit, visualize =  "individual")
 #'  }
